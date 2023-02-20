5b174f83e696b9d1c8933c7ae346f3e0dd6cf8ad
rui0323 <rui19990323@icloud.com>
2023-02-20 13:28:51 +0000

    コード整理

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CustomersController < ApplicationController
-   before_action :ensure_sign_in_admin
+   　#管理者としてログインしてないときはURLから入れない
+   　before_action :ensure_sign_in_admin
   def show
     @customer = Customer.find(params[:id])
     @posts = @customer.posts.page(params[:page]).per(7)
@@ -17,8 +18,8 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
     # is_deletedカラムをtrueに変更することにより削除フラグを立てる
     @customer.update(is_deleted: true)
-
     flash[:notice] = "退会処理を実行いたしました"
+   #管理者がユーザーを利用停止した場合会員一覧へ遷移する
     redirect_to admin_customers_path
   end
 
--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,9 @@
 class Admin::PostsController < ApplicationController
+   #管理者としてログインしてない場合はURLから入れない
    before_action :ensure_sign_in_admin
+
   def index
-    @posts = Post.all.page(params[:page])
+    @posts = Post.all.order(created_at: :desc).page(params[:page])
     @post = Post.new
     @customer = current_customer
   end
